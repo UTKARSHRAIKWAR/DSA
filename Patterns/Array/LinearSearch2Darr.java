6315c38f34f831199fe3eff829aa3530bc34ec14
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-06-28 19:39:22 +0530

    Arrays

---
+++ Patterns/Array/LinearSearch2Darr.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class LinearSearch2Darr {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows of array");
+        int rows = sc.nextInt();
+        System.out.println("Enter col of array");
+        int col = sc.nextInt();
+        int[][] array = new int[rows][col];
+
+        System.out.println("Enter element to be search");
+        int searchItem = sc.nextInt();
+
+        // input
+        // rows
+        for (int i = 0; i < rows; i++) {
+            // col
+            for (int j = 0; j < col; j++) {
+                System.out.println("Enter element " + i + " " + j);
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        // output
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                if (array[i][j] == searchItem) {
+                    System.out.println("Element found in row " + i + " col " + j);
+
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+
+    }
+}
--- Patterns/Array/TwoDarray.java
+++ Patterns/Array/TwoDarray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LinearSearch2Darray {
+public class TwoDarray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter rows of array");
