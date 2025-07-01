425a07ae9ba0f9bbbe70f5517b18d9f60264f0cc
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-07-01 17:59:41 +0530

    Sorting

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+public class InsertionSort {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 8, 4, 7 };
+        // time complexity = O(n^2)
+        // insertion sort
+
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (i >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = current;
+        }
+        printArray(arr);
+    }
+}
