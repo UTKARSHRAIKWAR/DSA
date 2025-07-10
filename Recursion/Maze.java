31ec0f3e15d8973a9b654f249e01288384b8c05b
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-07-10 23:14:58 +0530

    Recursion

---
+++ Recursion/Maze.java
@@ -0,0 +1,27 @@
+public abstract class Maze {
+
+    public static int countPath(int i, int j, int n, int m) {
+        if (i == n || j == m) {
+            return 0;
+        }
+
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        }
+
+        // move down
+        int downPath = countPath(i + 1, j, n, m);
+        // move right
+        int rightPath = countPath(i, j + 1, n, m);
+
+        return downPath + rightPath;
+
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+
+        int totalPath = countPath(0, 0, n, m);
+        System.out.println("Total Paths " + totalPath);
+    }
+}
