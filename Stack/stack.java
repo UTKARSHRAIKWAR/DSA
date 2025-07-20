8550075c146956260c136f836051215947681124
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-07-20 12:50:17 +0530

    Stack

--- Stack/stack.java
+++ Stack/stack.java
@@ -1,6 +1,29 @@
 import java.util.Stack;
 
 public class stack {
+
+    public static void pushAtBottom(int data, Stack<Integer> s) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+
+    }
+
+    public static void reverse(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top, s);
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
 
@@ -10,6 +33,8 @@ public class stack {
         s.push(2);
         s.push(6);
         s.push(4);
+        pushAtBottom(5, s);
+        reverse(s);
 
         while (!s.isEmpty()) {
             System.out.println(s.peek());
